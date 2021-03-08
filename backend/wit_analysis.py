561265af98b3a269373b8b5f9d934cc8e78c34de
junhup <junhup.lim@u.nus.edu>
2021-03-08 18:23:16 +0800

    fix bug on wit.ai when there is no intent

--- backend/wit_analysis.py
+++ backend/wit_analysis.py
@@ -25,7 +25,14 @@ class WitAnalysis:
             return None
         if len(sentiment['intents']) == 0:
             return ''
-        return sentiment['intents']
+        return sentiment['intents'][0]['name']
+    
+    def extract_intents_confidence(self, sentiment):
+        if not sentiment:
+            return None
+        if len(sentiment['intents']) == 0:
+            return ''
+        return sentiment['intents'][0]['confidence']
     
     def extract_traits(self, sentiment):
         if not sentiment:
@@ -34,13 +41,22 @@ class WitAnalysis:
             return ''
         return sentiment['traits']['wit$sentiment'][0]['value']
 
+    def extract_traits_confidence(self, sentiment):
+        if not sentiment:
+            return None
+        if len(sentiment['traits']) == 0:
+            return ''
+        return sentiment['traits']['wit$sentiment'][0]['confidence']
+
     def get_sentiments(self, master, top_tickers):
         tickers_sentiments = []
         for ticker in top_tickers:
             sentiment_df = master.loc[master['extracted'] == {ticker}]
             sentiment_df['sentiments'] = sentiment_df['title'].apply(self.extract_sentiments)
             sentiment_df['intents'] = sentiment_df['sentiments'].apply(self.extract_intents)
+            sentiment_df['intents_confidence'] = sentiment_df['sentiments'].apply(self.extract_intents_confidence)
             sentiment_df['traits'] = sentiment_df['sentiments'].apply(self.extract_traits)
+            sentiment_df['traits_confidence'] = sentiment_df['sentiments'].apply(self.extract_traits_confidence)
             tickers_sentiments.append((sentiment_df, ticker))
 
         for sentiment, ticker in tickers_sentiments:
